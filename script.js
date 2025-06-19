f7b21999084b6f8b076c9fde43850c14d23bf965
Praveen Patel <praveenpatel5957@gmail.com>
2025-06-19 22:44:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let tl = gsap.timeline();
+
+tl.from(".nav h3",{
+    y: -50,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.8,
+    stagger: 0.3
+})
+
+tl.from("#main h1",{
+    x: -500,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.8,
+    stagger: 0.4
+})
+
+tl.from('img', {
+    x: 100,
+    opacity: 0,
+    rotate: 45,
+    duration: 0.5,
+    stagger: 0.5
+})
